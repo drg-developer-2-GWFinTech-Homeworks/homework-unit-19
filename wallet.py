d14fc36e04980192b999562b5747f447139e125b
David Gayman <drg.developer.official@gmail.com>
2021-10-11 12:16:02 +0100

    Simplifying code.

--- wallet.py
+++ wallet.py
@@ -18,35 +18,12 @@ mnemonic = os.getenv("MNEMONIC", "gown field bottom essence blossom alone mounta
 
 def derive_wallets(mnemonic, coin_type):
 
-    result = subprocess.run("exit 0", shell=True, check=True)
-
-    # p = subprocess.Popen(["echo", "hello world"], stdout=subprocess.PIPE)
-    # out, err = p.communicate()
-    # print(out)
-
-    cmd = "echo"
-    args = "$SHELL"
-
-    # cmd = "hd-wallet-derive.php"
-    # args = f"-g --mnemonic=\"{mnemonic}\" --cols=path,address,privkey,pubkey --format=json --coin={coin_type} --numderive=3"
-
-    # command = [ "hd-wallet-derive.php", "-g", f"--mnemonic={mnemonic}", "--cols=path,address,privkey,pubkey", "--format=json}", f"--coin={coin_type}", "--numderive=3" ]
-    # command = f"./derive -g --mnemonic={mnemonic} --cols=path,address,privkey,pubkey --format=json --coin={coin_type} --numderive=3"
-    p = subprocess.Popen(args=args, executable=cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-
-    # str(os.getcwd())
-    # cur_dir + "/hd-wallet-derive.php ~~~"
-    # p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # (stdout, stderr) = p.communicate()
-
-    output, err = p.communicate()
-    p_status = p.wait()
-
+    cmd = f"hd-wallet-derive.php -g --mnemonic=\"{mnemonic}\" --cols=path,address,privkey,pubkey --format=json --coin={coin_type} --numderive=3"
+    result = subprocess.run(cmd, shell=True, check=True)
+    output = result.stdout
     keys = json.loads(output)
-    print(keys)
+    return keys
 
-    output_json = json.loads(output)
-    return output_json[coin_type]
 
 def extract_data_from_json(json_data):
     return (privKey)
@@ -68,4 +45,7 @@ def validate_key(coin_type, privKey):
 
 
 if __name__ == "__main__":
-    coins = derive_wallets(mnemonic, ETH)
+    coins = derive_wallets(mnemonic, BTC)
+    print(coins)
+    # output_json = json.loads(output)
+    # return output_json[coin_type]
